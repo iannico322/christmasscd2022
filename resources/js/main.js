1cf5ccd39c36d31c939ff24333e1c15c501ff968
iannico322 <73187566+iannico322@users.noreply.github.com>
2022-12-22 23:39:19 +0800

    sd

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,7 +4,7 @@ $(window).on('load',function(){
     $(".container").fadein(1000);
 })
 let music = document.querySelector(".music")
-music.play()
+music.play();
 
 setInterval( christmasCountDown, 1000);
 
@@ -19,30 +19,37 @@ var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-
-var countdown = (days < 10 ? "0" : "") + days + ":" + (hours < 10 ? "0" : "") + hours + ":" + (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
 document.querySelector(".days").innerHTML = (days < 10 ? "0" : "") + days + ":";
 document.querySelector(".hours").innerHTML = (hours < 10 ? "0" : "") + hours + ":" ;
 document.querySelector(".minutes").innerHTML = (minutes < 10 ? "0" : "") + minutes + ":";
 document.querySelector(".seconds").innerHTML = (seconds < 10 ? "0" : "") + seconds;
 
 
-if(distance < 00){
-    music.src = "./resources/media/audio/alliwant.mp3"
-    music.play()
-    document.querySelector(".label").innerHTML = "Caulin Family";
+if(distance > 00){
+    
+    
+   
+    
+    document.querySelector(".label").innerHTML = "Merry Christmas"
     document.querySelector(".fire-display").style.display = "block";
     var greet = document.querySelector(".countdown")
     greet.className += " greet"
-    greet.innerHTML = "Merry Christmas"
+    greet.innerHTML = "Caulin Family";
     
-    
+    setTimeout(function() {
+        music.src = "./resources/media/audio/alliwant.mp3"     
+        music.play()     
+        console.log("sing")            
+    }, 1000);
+                     
+   
 
 
 }
 // Update the countdown element on the page
 
 }
+
 
 
 
